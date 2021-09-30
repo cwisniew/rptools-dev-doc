546776e859eea3946eabef42de547e77128fa128
Craig Wisniewski <craig.wisniewski@gmail.com>
2021-09-30 13:10:20 +0930

    add blog post for speech name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,14 +21,16 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
+          remarkPlugins: [require('mdx-mermaid')],
           // Please change this to your repo.
-          editUrl: 'https://github.com/cwisniew/rptools-dev-doc/edit/main/website/',
+          editUrl: 'https://github.com/cwisniew/rptools-dev-doc/edit/main/',
         },
         blog: {
           showReadingTime: true,
+          remarkPlugins: [require('mdx-mermaid')],
           // Please change this to your repo.
           editUrl:
-            'https://github.com/cwisniew/rptools-dev-doc/edit/main/website/blog/',
+            'https://github.com/cwisniew/rptools-dev-doc/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
